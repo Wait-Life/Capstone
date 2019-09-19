35cc946fd34831f7b5c11566fc08cba5a62392b8
Steven Turner <steven.turner12x@gmail.com>
2019-09-19 16:09:15 -0500

    initial push

---
+++ src/main/java/com/codeup/drinkhustle/DrinkhustleApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.drinkhustle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkhustleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrinkhustleApplication.class, args);
+    }
+
+}
