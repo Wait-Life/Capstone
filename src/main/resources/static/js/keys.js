b1bf4def3439b8103bd435bddfccd1f2049cbb9d
AliciaMonsibais <alicia.monsibais1@gmail.com>
2019-09-30 12:18:07 -0500

    fixing breaking issue

--- src/main/resources/static/js/keys.js
+++ src/main/resources/static/js/keys.js
@@ -1,2 +1,9 @@
 var fsKey = 'AiliJZQ5oTTaLvOkVthAgz';
 var mapBoxKey = 'pk.eyJ1IjoiYWxpY2lhbW9uc2liYWlzIiwiYSI6ImNqemVqMWZoMjAyeWIzbGxrZWtzdTd1MXYifQ.cayJDxxqrvlLlBNaQYCsVw';
+var fBKey = '413668279352778';
+
+// SID//API:  SK93270ed8dfcc39fb0b0546a3b6469be1;
+// SECRET: Ccby76RTXUrpDO0eKTHJuXYwmjtt1yYN;
+
+var twilioSID = "AC583d503138def878fda30f5b0d7136dc";
+var twilioToken = "28fd720960a43b91d8f80c6c7909428d";
